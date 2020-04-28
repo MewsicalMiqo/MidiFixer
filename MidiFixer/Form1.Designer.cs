51a77746aabe52682a136dfbda256d7166ce7cdc
MewsicalMiqo <miqomusicplayer@gmail.com>
2020-04-27 20:15:28 -0400

    Add project files.

---
+++ MidiFixer/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace MidiFixer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(681, 297);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Open/drag a MIDI file";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(13, 324);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(160, 40);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(179, 324);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(160, 40);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 376);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "MIDI File Fixer";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FormDragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FormDragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ MidiFixer/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+
+namespace MidiFixer
+{
+    public partial class Form1 : Form
+    {
+        MidiFile song;
+        string filePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void FormDragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        void FormDragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i].EndsWith(".mid"))
+                {
+                    LoadMIDIFromFile(files[i]);
+                    break;
+                }
+            }
+        }
+
+        void LoadMIDIFromFile(string path)
+        {
+            song = MidiFile.Read(path);
+            filePath = path;
+            textBox1.Text = "MIDI File: " + Path.GetFileName(path) + Environment.NewLine;
+            textBox1.AppendText("Type: " + song.OriginalFormat + Environment.NewLine);
+            textBox1.AppendText("Channels: " + song.GetChannels().Count() + Environment.NewLine);
+            ReadTracksFromFile(song);
+        }
+
+        private void WriteMidiToFile(MidiFile inputFile, string fileName)
+        {
+            inputFile.Write(fileName, true, MidiFileFormat.MultiTrack,
+               new WritingSettings { CompressionPolicy = CompressionPolicy.Default });
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MIDI files (*.mid)|*.mid";
+            openFileDialog.RestoreDirectory = true;
+
+            string filePath = default;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+                LoadMIDIFromFile(filePath);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var chunks = song.GetTrackChunks();
+            var tempoMap = song.GetTempoMap();
+            var channels = song.GetChannels();
+
+            var fixedMidi = new MidiFile();
+            var headerChunk = new TrackChunk();
+
+            var headerEvents = chunks.ElementAt(0).GetTimedEvents();
+            var titleEvent = new List<TimedEvent>();
+            foreach (var evt in headerEvents)
+            {
+                if (evt.Event.EventType == MidiEventType.SequenceTrackName)
+                {
+                    var txtEvent = evt.Event as SequenceTrackNameEvent;
+                    titleEvent.Add(evt);
+                    break;
+                }
+            }
+            headerChunk.AddTimedEvents(titleEvent);
+
+            fixedMidi.Chunks.Add(headerChunk);
+            fixedMidi.ReplaceTempoMap(tempoMap);
+
+            var notes = song.GetNotes();
+            for (int i = 0; i < channels.Count(); i++)
+            {
+                var trackNotes = notes.Where(n => n.Channel == i);
+                if (trackNotes.Count() > 0)
+                {
+                    var trackChunk = new TrackChunk();
+                    trackChunk.AddNotes(trackNotes);
+                    var newTimedEvents = GetTimedEventsByChannel(chunks, i);
+                    trackChunk.AddTimedEvents(newTimedEvents);
+                    fixedMidi.Chunks.Add(trackChunk);
+                }
+            }
+            
+            string outputFilename = Path.GetFileNameWithoutExtension(filePath) + "_fix.mid";
+            WriteMidiToFile(fixedMidi, outputFilename);
+            textBox1.AppendText("Wrote to file: " + outputFilename + Environment.NewLine);
+        }
+
+        private void ReadTracksFromFile(MidiFile song)
+        {
+            var chunks = song.GetTrackChunks();
+
+            for (int i = 0; i < chunks.Count(); i++)
+            {
+                textBox1.AppendText("Track " + i + ": ");
+                var channels = chunks.ElementAt(i).GetChannels();
+                foreach (var s in channels)
+                    textBox1.AppendText("ch" + s + " ");
+                textBox1.AppendText(Environment.NewLine);
+            }
+        }
+
+        private List<TimedEvent> GetTimedEventsByChannel(IEnumerable<TrackChunk> chunks, int ch)
+        {
+            var timedEvents = chunks.GetTimedEvents();
+            var newTimedEvents = new List<TimedEvent>();
+
+            foreach (var timedEvent in timedEvents)
+            {
+                var pc = timedEvent.Event as ProgramChangeEvent;
+                if (pc != null && pc.Channel == ch)
+                    newTimedEvents.Add(timedEvent);
+            }            
+            foreach (var c in chunks)
+            {
+                var channels = c.GetChannels();
+                if (channels.Contains((FourBitNumber)ch))
+                {
+                    var textEvents = c.GetTimedEvents().Where(evt => evt.Event.EventType == MidiEventType.SequenceTrackName);
+                    if (textEvents.Count() == 0)
+                        continue;
+                    else
+                        foreach (var te in textEvents)
+                            newTimedEvents.Add(te);
+                }
+            }
+            return newTimedEvents;
+        }
+    }
+}
